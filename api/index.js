b5acfa54f085087024a1dfdffae1ce1d58e51cbf
Your Name <you@example.com>
2023-10-09 22:42:22 +0700

    chat-app

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ const bcryptSalt = bcrypt.genSaltSync(10)
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://mernchat:GGEapYGMx97mg7oU@cluster0.kbhywfz.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(
   cors({
-    origin: clientUrl,
+    origin: "https://chatkal-client.vercel.app",
     methods: ["POST", "GET"],
     credentials: true,
   })
@@ -49,7 +49,7 @@ async function getUserDataFromRequest(req) {
   return new Promise((resolve, reject) =>{
     const token = req.cookies?.token;
     if (token) {
-      jwt.verify(token,jwtSecret, {}, (err, userData) => {
+      jwt.verify(token,'dwadjawdkjj2103fdmmddwkad2', {}, (err, userData) => {
         if (err) throw err;
         resolve(userData)
       })
@@ -86,7 +86,7 @@ app.get("/people", async (req, res) => {
 app.get("/profile", (req, res) => {
   const token = req.cookies?.token
   if (token) {
-    jwt.verify(token, jwtSecret, {}, (err, userData) => {
+    jwt.verify(token, 'dwadjawdkjj2103fdmmddwkad2', {}, (err, userData) => {
       if (err) throw err
       res.json(userData)
     })
@@ -103,7 +103,7 @@ app.post('/login', async (req,res) => {
   if (foundUser) {
    const passOk = bcrypt.compareSync(password, foundUser.password)
    if (passOk) {
-    jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (err, token) => {
+    jwt.sign({ userId: foundUser._id, username }, 'dwadjawdkjj2103fdmmddwkad2', {}, (err, token) => {
       res.cookie("token", token, { sameSite:"none", secure:true }).json({
         id : foundUser._id
       })
@@ -123,7 +123,7 @@ app.post("/register", async (req, res) => {
   try {
     const hashedPassword = bcrypt.hashSync(password, bcryptSalt)
     const createdUser = await User.create({ username: username, password:hashedPassword  });
-    jwt.sign({ userId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
+    jwt.sign({ userId: createdUser._id, username }, 'dwadjawdkjj2103fdmmddwkad2', {}, (err, token) => {
       if (err) throw err;
       res.cookie("token", token, { sameSite:"none", secure:true }).status(201).json({
         id : createdUser._id,
@@ -173,7 +173,7 @@ wss.on('connection', (connection, req) => {
     if (tokenCookiesString) {
       const token = tokenCookiesString.split("=")[1]
       if (token) {
-        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+        jwt.verify(token, 'dwadjawdkjj2103fdmmddwkad2', {}, (err, userData) => {
           if (err) throw err
           const {userId, username} = userData
           connection.userId = userId
