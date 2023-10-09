643cb00e0bd96a3bdc92b9eca29336a076422f7e
Your Name <you@example.com>
2023-10-09 21:27:39 +0700

    chat-app

--- api/index.js
+++ api/index.js
@@ -38,8 +38,9 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(
   cors({
+    origin: [clientUrl],
+    methods: ["POST", "GET"],
     credentials: true,
-    origin: clientUrl,
   })
 );
 
