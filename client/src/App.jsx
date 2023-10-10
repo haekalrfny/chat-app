c608f82c30f0cd2a52e93b27a8c2bb8a5bc87b82
Your Name <you@example.com>
2023-10-11 06:14:48 +0700

    chat-app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = "https://chatkal-api.vercel.app";
+  axios.defaults.baseURL = "http://localhost:4040";
   axios.defaults.withCredentials = true;
 
   return (
